88cd18c48b1ed9a61dae5f87d3348370836b5d3b
Moulin Nathan <nathan.moulin718@gmail.com>
2026-04-03 10:13:14 +0200

    Add unreal project

---
+++ Source/IcanExamenT2/IcanExamenT2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IcanExamenT2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IcanExamenT2, "IcanExamenT2" );
